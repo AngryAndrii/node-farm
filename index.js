b49276655d71e7562d6a67ef4313c821586fe599
Andrii Chychkan <mywork1728@gmail>
2024-08-19 22:33:14 +0300

    added some work with libraries

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const http = require("http");
 const path = require("path");
 const url = require("url");
+const slugify = require("slugify");
 const replaseTamplate = require("./modules/replaseTamplate");
 
 ////////////////////////////////
@@ -44,8 +45,6 @@ const tempProduct = fs.readFileSync(
 );
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-
-const dataObject = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
